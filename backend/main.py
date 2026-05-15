9f09a0d0a88d87b683a1d551941386aa37570bc7
Drivve15042025 <you@example.com>
2026-05-15 11:07:27 -0700

    main file fix

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ from sqlalchemy.orm import Session
 import uvicorn
 
 from drivve_api.app_config import create_app
-from drivve_api.createprofile import EMAIL_USER, send_email
+from drivve_api.createprofile import EMAIL_USER
 from database import engine, get_db, Base
 
 # =========================
