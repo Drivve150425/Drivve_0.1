3a0c2982b1d569ffa1256a26de261e919447b390
Drivve15042025 <you@example.com>
2026-05-18 13:08:23 -0700

    port fix

--- backend/drivve_api/createprofile.py
+++ backend/drivve_api/createprofile.py
@@ -102,8 +102,7 @@ async def send_email_otp(request: EmailOTPRequest):
 
         msg.attach(MIMEText(html, "html"))
 
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
+        server = smtplib.SMTP("smtp.gmail.com", 465)
         server.login(EMAIL_USER, EMAIL_PASSWORD)
         server.sendmail(
             EMAIL_USER,
